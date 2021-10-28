b5abfb78bb1871049df8b9c90463c40fc0ae7376
Arthuralentejo <arthur.alentejo228@gmail.com>
2021-10-28 02:54:07 -0300

    Adicionando tratamendo de erro e Cors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,33 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    return res.status(200).send({})
+  }
+  next()
+})
+
 app.use('/student', require('./routes/studentRoutes'))
 app.use('/user', require('./routes/userRoutes'))
+
+// Rota não encontrada
+app.use((req, res, next) => {
+  const error = new Error('Not found')
+  error.status = 404
+  next(error)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.send({
+    error: {
+      message: error.message
+    }
+  })
+})
 
 module.exports = app
