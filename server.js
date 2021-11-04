5aa7b809496302913f4a026f158fe48455c44a72
Arthuralentejo <arthur.alentejo228@gmail.com>
2021-11-04 01:02:06 -0300

    Alterando caminho do server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config({
   path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 })
 
-const app = require('./app')
+const app = require('./src/app')
 
 require('dotenv').config()
 
