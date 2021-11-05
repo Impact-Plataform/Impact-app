12c077fdc4ce2c28c7ef637a331f5d103635e9ca
Arthuralentejo <arthur.alentejo228@gmail.com>
2021-11-05 17:05:29 -0300

    Retornando estudantes

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -4,8 +4,8 @@ const studentController = require('../controllers/studentController')
 const { required, adminOnly } = require('../middleware/auth')
 
 router.post('/saveStudents', required, adminOnly, studentController.save)
-router.get('/getStudent/:id', required, studentController.read)
-router.get('/getAllStudents', required, studentController.readAll)
+router.get('/getStudent/:id', studentController.read)
+router.get('/getAllStudents', studentController.readAll)
 router.put('/updateStudents/:id', required, adminOnly, studentController.update)
 router.delete('/deleteStudents/:id', required, adminOnly, studentController.delete)
 
